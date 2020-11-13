2826ab619e811ed66a82a4655496aa6ac8761610
killermitch <anthonnymitch22@gmail.com>
2020-11-12 20:33:53 -0500

    home page finish we just need to change the photos and the text

--- client/src/pages/home.js
+++ client/src/pages/home.js
@@ -58,7 +58,7 @@ class Home extends React.Component {
               expand='md'
               scrolling
               fixed='top'
-          
+
             >
               <MDBContainer>
                 <MDBNavbarBrand>
@@ -81,7 +81,7 @@ class Home extends React.Component {
                   </MDBNavbarNav>
                   <MDBNavbarNav right>
                     <MDBNavItem>
-                    <MDBBtn color='green'>Login</MDBBtn>
+                      <MDBBtn color='green'>Login</MDBBtn>
                     </MDBNavItem>
                   </MDBNavbarNav>
                 </MDBCollapse>
@@ -108,7 +108,7 @@ class Home extends React.Component {
                 </h1>
                 <hr className='hr-light' />
                 <h6 className='mb-4'>
-                 Aqui vamos a poner el mission estatement del app y cuando le den click al button learn more los van a moder a un nivel de la paginadonde les explicares como funciona cada herramienta. 
+                  Aqui vamos a poner el mission estatement del app y cuando le den click al button learn more los van a moder a un nivel de la paginadonde les explicares como funciona cada herramienta.
                 </h6>
                 <MDBBtn outline color='white'>
                   Learn More
@@ -144,7 +144,7 @@ class Home extends React.Component {
                       />
                       <div className='text-center mt-4 black-text'>
                         <MDBBtn color='green'>Sign Up</MDBBtn>
-                    
+
                       </div>
                     </MDBCardBody>
                   </MDBCard>
@@ -154,24 +154,247 @@ class Home extends React.Component {
           </MDBContainer>
         </MDBView>
 
-        <MDBContainer>
+        <MDBContainer id='mission'>
           <MDBRow className='py-5'>
-            <MDBCol md='12' className='text-center'>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
-                enim ad minim veniam, quis nostrud exercitation ullamco laboris
-                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
-                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
-                nulla pariatur. Excepteur sint occaecat cupidatat non proident,
-                sunt in culpa qui officia deserunt mollit anim id est laborum.
-              </p>
+            <MDBCol md='12' className='white-text text-center'>
+              <h3>
+                <MDBIcon icon="quote-left" />          AQUI VAMOS A PONER UNA FRASE O UN LEMA PRA LA MARCA, O PODEMOS HACER COMO UN PEQUENO MISSION STATEMENT.                 <MDBIcon icon="quote-right" />
+              </h3>
             </MDBCol>
           </MDBRow>
         </MDBContainer>
+
+
+        <section className="my-5" id='tools'>
+          <h1 className="h1-responsive font-weight-bold text-center my-5">
+            <strong>Explore The Tribe</strong>
+          </h1>
+          <h4 className="white-text w-responsive text-center mx-auto mb-5">
+            Aqui se va a explicar que WOD tribe no es solo para ver los ejercicios si no que pueden hacer otras cosas.
+        </h4>
+
+          <MDBRow>
+            <MDBCol lg="5" className="mb-lg-0 mb-5">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Others/images/83.jpg"
+                alt=""
+                className="img-fluid rounded z-depth-1"
+              />
+            </MDBCol>
+            <MDBCol lg="7">
+              <h3 >  <MDBIcon icon="dumbbell" />         Check your progress</h3>
+              <h4>
+                Aqui se hablara acerca de los WOD y como pueden ver sus resultados y lo que han mejorado
+           </h4>
+            </MDBCol>
+          </MDBRow>
+          <hr className="my-5" />
+          <MDBRow>
+            <MDBCol lg="7">
+              <h3>  <MDBIcon icon="utensils" />        Track your calories</h3>
+              <h4>
+                Aqui se hablara acerca de calorie ytacker y como funciona
+           </h4>
+
+            </MDBCol>
+            <MDBCol lg="5">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Others/images/82.jpg"
+                alt=""
+                className="img-fluid rounded z-depth-1"
+              />
+            </MDBCol>
+          </MDBRow>
+          <hr className="my-5" />
+          <MDBRow>
+            <MDBCol lg="5" className="mb-lg-0 mb-5">
+              <img
+                src="https://mdbootstrap.com/img/Photos/Others/images/83.jpg"
+                alt=""
+                className="img-fluid rounded z-depth-1"
+              />
+            </MDBCol>
+            <MDBCol lg="7">
+              <h3>  <MDBIcon icon="calculator" />          Fitness Calculator</h3>
+              <h4>
+                Aqui se hablara caerca de como puedes usar la calculadora para ver BMI y toda esta paja
+           </h4>
+            </MDBCol>
+          </MDBRow>
+          <hr className="my-5" />
+        </section>
+
+        <MDBCard className="my-5 px-1 pb-5 text-center">
+          <MDBCardBody>
+            <h2 className="h1-responsive font-weight-bold my-5">
+              Our amazing team
+          </h2>
+            <p className="white-text w-responsive mx-auto mb-5">
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Fugit,
+              error amet numquam iure provident voluptate esse quasi, veritatis
+              totam voluptas nostrum quisquam eum porro a pariatur veniam.
+          </p>
+            <MDBRow>
+              <MDBCol md="6" className="mb-md-0 mb-5">
+                <img
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(10).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                />
+                <h4 className="font-weight-bold white-text my-4">
+                  Anthonny Rodriguez 
+              </h4>
+                <h6 className="text-uppercase white-text mb-3">Full-Stack Developer</h6>
+                <MDBBtn tag="a" floating size="sm" className="mx-1 mb-0 btn-green">
+                  <MDBIcon fab icon="facebook-f" />
+                </MDBBtn>
+                <MDBBtn
+                  tag="a"
+                  floating
+                  size="sm"
+                  className="mx-1 mb-0 btn-green"
+                >
+                  <MDBIcon fab icon="github" />
+                </MDBBtn>
+                <MDBBtn tag="a" floating size="sm" className="mx-1 mb-0 btn-green">
+                  <MDBIcon fab icon="twitter" />
+                </MDBBtn>
+              </MDBCol>
+
+
+              <MDBCol md="4" className="mb-md-0 mb-5">
+                <img
+                  tag="img"
+                  src="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+                  className="rounded-circle z-depth-1 img-fluid"
+                  alt="Sample avatar"
+                />
+                <h4 className="font-weight-bold white-text my-4">
+                  Andrew Rodriguez 
+              </h4>
+              <h6 className="text-uppercase white-text mb-3">Full-Stack Developer</h6>
+                <MDBBtn tag="a" floating size="sm" className="mx-1 mb-0 btn-green">
+                  <MDBIcon fab icon="facebook-f" />
+                </MDBBtn>
+                <MDBBtn
+                  tag="a"
+                  floating
+                  size="sm"
+                  className="mx-1 mb-0 btn-green"
+                >
+                  <MDBIcon fab icon="github" />
+                </MDBBtn>
+                <MDBBtn tag="a" floating size="sm" className="mx-1 mb-0 btn-green">
+                  <MDBIcon fab icon="twitter" />
+                </MDBBtn>
+              </MDBCol>
+            </MDBRow>
+          </MDBCardBody>
+        </MDBCard>
+        <section className="my-5">
+      <h2 className="h1-responsive font-weight-bold text-center my-5 white-text">
+        Contact us
+      </h2>
+    
+      <MDBRow>
+        <MDBCol lg="5" className="lg-0 mb-4">
+          <MDBCard>
+            <MDBCardBody>
+              <div>
+                <h3 className="mt-2">
+                  <MDBIcon icon="envelope" /> Write to us:
+                </h3>
+              </div>
+              <p className="white-text">
+                We'll write rarely, but only the best content.
+              </p>
+              <div className="md-form">
+                <MDBInput
+                  icon="user"
+                  label="Your name"
+                  iconClass="white-text"
+                  type="text"
+                  id="form-name"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="envelope"
+                  label="Your email"
+                  iconClass="white-text"
+                  type="text"
+                  id="form-email"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="tag"
+                  label="Subject"
+                  iconClass="white-text"
+                  type="text"
+                  id="form-subject"
+                />
+              </div>
+              <div className="md-form">
+                <MDBInput
+                  icon="pencil-alt"
+                  label="Icon Prefix"
+                  iconClass="white-text"
+                  type="textarea"
+                  id="form-text"
+                />
+              </div>
+              <div className="text-center">
+                <MDBBtn color="green">Submit</MDBBtn>
+              </div>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
+        <MDBCol lg="7">
+          <div
+            id="map-container"
+            className="rounded z-depth-1-half map-container"
+            style={{ height: "400px" }}
+          >
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d76765.98321148289!2d-73.96694563267306!3d40.751663750099084!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1spl!2spl!4v1525939514494"
+              title="This is a unique title"
+              width="100%"
+              height="100%"
+              frameBorder="0"
+              style={{ border: 0 }}
+            />
+          </div>
+          <br />
+          <MDBRow className="text-center white-text">
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="green" className="accent-1">
+                <MDBIcon icon="map-marker-alt" />
+              </MDBBtn>
+              <p>New York, 94126</p>
+              <p className="mb-md-0">United States</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="green" className="accent-1">
+                <MDBIcon icon="phone" />
+              </MDBBtn>
+              <p>+ 01 234 567 89</p>
+              <p className="mb-md-0">Mon - Fri, 8:00-22:00</p>
+            </MDBCol>
+            <MDBCol md="4">
+              <MDBBtn tag="a" floating color="green" className="accent-1">
+                <MDBIcon icon="envelope" />
+              </MDBBtn>
+              <p>info@gmail.com</p>
+              <p className="mb-md-0">sale@gmail.com</p>
+            </MDBCol>
+          </MDBRow>
+        </MDBCol>
+      </MDBRow>
+    </section>
       </div>
     );
   }
 }
-
 export default Home;
