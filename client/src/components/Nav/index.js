078e0637ffe668d3069b645ece4812e04e2eb24d
killermitch <anthonnymitch22@gmail.com>
2020-11-16 15:32:23 -0500

    navbar fixed but still have to make the navbar button to work when seen in mobile mode

--- client/src/components/Nav/index.js
+++ client/src/components/Nav/index.js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState, useContext } from "react";
 import { Link } from "react-router-dom";
 import "./style.css";
-import {MDBIcon} from 'mdbreact';
+import { MDBIcon } from 'mdbreact';
 import AuthButton from "../AuthButton";
 import { UserContext } from "../../utils/UserContext";
 //I want to add some basic inline styling here, even though we are bringing in styles
@@ -23,9 +23,9 @@ function Nav() {
     setWidth(window.innerWidth)
   };
 
-  // const toggleNav = () => {
-  //   setOpen(!open);
-  // };
+  const toggleNav = () => {
+    setOpen(!open);
+  };
 
   useEffect(() => {
 
@@ -40,22 +40,29 @@ function Nav() {
 
   return (
     <nav className="navbar navbar-expand-lg navbar-dark bg-dark mb-2 fixed-top">
-      <Link className="navbar-brand" to="/">
-      <MDBIcon fab icon="mandalorian" /> WOD Tribe 
-        </Link>
-      <div className={`${open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
-       
-        <ul className="navbar-nav ml-auto">
-          <li className="nav-item ">
-            <Link style={buttonStyle} className=" btn btn-dark" to="/public">Home Page</Link>
-            <Link style={buttonStyle} className="btn btn-dark" to="/protected">User Page </Link>
-            {user.username ? "" :
-              <Link style={buttonStyle} className="btn btn-dark" to="/register">Register</Link>
-            }
-            <AuthButton />
-          </li>
 
-        </ul>
+      <Link className="navbar-brand" to="/">
+
+        <MDBIcon fab icon="mandalorian" /> WOD Tribe
+        </Link>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarSupportedContent">
+        <div className={`${open ? "" : "collapse "}navbar-collapse`} id="navbarNav">
+
+          <ul className="navbar-nav ml-auto">
+            <li className="nav-item ">
+              <Link style={buttonStyle} className=" btn btn-dark" to="/public">Home Page</Link>
+              <Link style={buttonStyle} className="btn btn-dark" to="/protected">User Page </Link>
+              {user.username ? "" :
+                <Link style={buttonStyle} className="btn btn-dark" to="/register">Register</Link>
+              }
+              <AuthButton />
+            </li>
+
+          </ul>
+        </div>
       </div>
     </nav>
   );
