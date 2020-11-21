f19cd4eaa1775cda4310da2a637c15fe1f410bf7
killermitch <anthonnymitch22@gmail.com>
2020-11-21 17:54:37 -0500

    background added to workouts page, and footer added to all pages, jumbotron background change

--- client/src/pages/Workouts.js
+++ client/src/pages/Workouts.js
@@ -5,6 +5,7 @@ import API from "../utils/API";
 import { Link } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import { List, ListItem } from "../components/List";
+import { MDBCol, MDBContainer, MDBRow, MDBFooter } from "mdbreact";
 import { Input, TextArea, FormBtn } from "../components/Form";
 import '../pages/workouts.css'
 function Workouts() {
@@ -55,11 +56,12 @@ function Workouts() {
   };
 
     return (
-      <Container fluid id='workouts'>
+      <div id='workouts'>
+      <Container fluid >
         <Row>
           <Col size="md-6">
-            <Jumbotron>
-              <h2>Create The Workout of the day</h2>
+            <Jumbotron  id="jumbo">
+              <h2>Create a WOD</h2>
             </Jumbotron>
             <form>
               <Input
@@ -75,7 +77,7 @@ function Workouts() {
               <TextArea
                 onChange={handleInputChange}
                 name="details"
-                placeholder="Details (Optional)"
+                placeholder="Details (required)"
               />
               <FormBtn
                 disabled={!(formObject.weigth && formObject.muscle)}
@@ -86,8 +88,8 @@ function Workouts() {
             </form>
           </Col>
           <Col size="md-6 sm-12">
-            <Jumbotron>
-              <h1>My Workouts</h1>
+            <Jumbotron id="jumbo">
+              <h2>My WOD</h2>
             </Jumbotron>
             {workouts.length ? (
               <List>
@@ -108,6 +110,46 @@ function Workouts() {
           </Col>
         </Row>
       </Container>
+      <MDBFooter color="#" className="font-small darken-3 pt-0">
+      <MDBContainer>
+        <MDBRow>
+          <MDBCol md="12" className="py-5">
+            <div className="mb-5 flex-center">
+              <a className="fb-ic">
+                <i className="fab fa-facebook-f fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="tw-ic">
+                <i className="fab fa-twitter fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="gplus-ic">
+                <i className="fab fa-google-plus fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="li-ic">
+                <i className="fab fa-linkedin-in fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="ins-ic">
+                <i className="fab fa-instagram fa-lg white-text mr-md-5 mr-3 fa-2x">
+                </i>
+              </a>
+              <a className="pin-ic">
+                <i className="fab fa-pinterest fa-lg white-text fa-2x"> </i>
+              </a>
+            </div>
+          </MDBCol>
+        </MDBRow>
+      </MDBContainer>
+      <div className="footer-copyright text-center py-3">
+        <MDBContainer fluid>
+          &copy; {new Date().getFullYear()} Copyright:{" "}
+          <a href="">WOD Tribe Group Inc </a>
+        </MDBContainer>
+      </div>
+    </MDBFooter>
+      </div>
     );
   }
 
