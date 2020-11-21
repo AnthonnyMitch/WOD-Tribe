56b6c840dfab16d9c594f4fe8a312fb98f4e8ea1
killermitch <anthonnymitch22@gmail.com>
2020-11-21 14:39:43 -0500

    css added to homepage

--- client/src/pages/PublicRoute/index.js
+++ client/src/pages/PublicRoute/index.js
@@ -63,12 +63,13 @@ function PublicRoute() {
         <MDBRow>
           <MDBCol lg="5" className="mb-lg-0 mb-5">
             <img
-              src="https://images.squarespace-cdn.com/content/v1/5a57e930f9a61ed005b80c45/1515713915152-TBG1NUV2DUAKR8VIC5M4/ke17ZwdGBToddI8pDm48kDS1ERbUEGbRuV0gI1MhqsAUqsxRUqqbr1mOJYKfIPR7LoDQ9mXPOjoJoqy81S2I8N_N4V1vUb5AoIIIbLZhVYxCRW4BPu10St3TBAUQYVKcTDMZdXjHNZqplaNSeVjrXHJclnzbXwJyYZyWxDXN4DBgGwjbkKGcQepkTI2TJ4nb/Crossfit-banner-1.jpg?format=2500w"
+              src="https://images.unsplash.com/photo-1597075958252-60fc09ec20c2?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MzV8fGd5bSUyMGVxdWlwbWVudHxlbnwwfHwwfA%3D%3D&auto=format&fit=crop&w=500&q=60"
               alt=""
+              id="imageWod"
               className="img-fluid rounded z-depth-1 text-center"
             />
           </MDBCol>
-          <MDBCol lg="7">
+          <MDBCol lg="7" id="WodBorder">
             <h3 >  <MDBIcon icon="dumbbell" />  Check The Workout of the Day</h3>
             <h4 className='white-text'>
             A written workout plan not only provides a layout for the day, but it also sets up the big picture for weeks or months to come. In other words, an established plan dictates the work that must be done in order to reach the goal.
@@ -77,15 +78,16 @@ function PublicRoute() {
         </MDBRow>
         <hr className="my-5" />
         <MDBRow>
-          <MDBCol lg="7">
+          <MDBCol lg="7" id="WodBorder">
             <h3>  <MDBIcon icon="utensils" />        Track your calories</h3>
             <h4  className='white-text'>
               Tracking your daily calories is a very important part of your fitness journey, that's why on this section you can track your breakfast, lunch and dinner calories just by typing its ingredients.</h4>
           </MDBCol>
           <MDBCol lg="5">
             <img
-              src="https://sipipa.com/blog/wp-content/uploads/2018/07/counting-calories-watching-what-you-eat.jpg"
+              src="https://images.unsplash.com/photo-1498837167922-ddd27525d352?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxzZWFyY2h8MTd8fGhlYWx0aHklMjBmb29kfGVufDB8fDB8&auto=format&fit=crop&w=500&q=60"
               alt=""
+              id="imageCalories"
               className="img-fluid rounded z-depth-1 text-center"
             />
           </MDBCol>
@@ -96,10 +98,11 @@ function PublicRoute() {
             <img
               src="https://www.setantacollege.com/wp-content/uploads/2017/02/performance_science_home.jpg"
               alt=""
+              id="imageCalculator"
               className="img-fluid rounded z-depth-1"
             />
           </MDBCol>
-          <MDBCol lg="7">
+          <MDBCol lg="7" id="WodBorder" >
             <h3>  <MDBIcon icon="calculator" />          Fitness Calculator</h3>
             <h4  className='white-text'>
              On this section, we offers a number of free tools to help you determine everything from your body mass index (BMI) to your body fat composition, daily caloric needs and more. Each of those tools will help you find an exercise program that fits your individual needs and goals, whether that be losing weight or increasing your strength with weight training.
@@ -111,7 +114,7 @@ function PublicRoute() {
 
 
 
-      <MDBRow className=" text-center">
+      <MDBRow className=" text-center" id="team">
         <MDBCardBody>
           <h2 className="h1-responsive font-weight-bold my-5  white-text">
             Our amazing team
